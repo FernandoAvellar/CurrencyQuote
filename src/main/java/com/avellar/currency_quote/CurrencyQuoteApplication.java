596921df534a7e58484fb5db1efdd0bba8cafac9
Fernando Avellar Jr. <favellar@yahoo.com.br>
2024-07-09 09:36:27 -0300

    First commit

---
+++ src/main/java/com/avellar/currency_quote/CurrencyQuoteApplication.java
@@ -0,0 +1,13 @@
+package com.avellar.currency_quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyQuoteApplication.class, args);
+	}
+
+}
